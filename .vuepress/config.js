114a6c64026f85edb440c4255bfb4bde564697f0
wangt <wangtong@cbim.com.cn>
2021-06-30 09:29:01 +0800

    添加樱花飘落插件

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -210,5 +210,19 @@ module.exports = {
       }
     ],
 
+    [
+      // 樱花飘落
+      "sakura",
+      {
+        num: 20,  // 默认数量
+        show: true, //  是否显示
+        zIndex: -1,   // 层级
+        img: {
+          replace: false,  // false 默认图 true 换图 需要填写httpUrl地址
+          httpUrl: '...'     // 绝对路径
+        }
+      }
+    ],
+
   ]
 }
